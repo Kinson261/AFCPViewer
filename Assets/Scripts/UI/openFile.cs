4f20a7cd47c8e6fa7de74149ca56ba1129fa6a15
Kinson261 <80491985+Kinson261@users.noreply.github.com>
2022-07-20 05:48:26 +0300

    added func switchView 1 and 2  for switching to cut view

--- Assets/Scripts/UI/openFile.cs
+++ Assets/Scripts/UI/openFile.cs
@@ -9,7 +9,7 @@ using Dummiesman;
 
 public class openFile : MonoBehaviour
 {
-    GameObject loadedObject;
+    public GameObject loadedObject;
     private string _path;
     string[] path;
     string error = string.Empty;
@@ -41,6 +41,64 @@ public class openFile : MonoBehaviour
             loadedObject.GetComponent<Renderer>().material.color = Color.white;
 
         }
+    }
+
+    public void switchView(){
+        //path = Resources.Load("objSwitch") as string;
+        path[0] = Application.streamingAssetsPath + "/Model3d/" + "wheel_1.obj";
+
+        if (!File.Exists(path[0])){
+            error = "File " + path[0] + " does not exist";
+        }
+        else{
+            if (loadedObject != null){
+                Destroy(loadedObject);
+            }
+            loadedObject = new OBJLoader().Load(path[0]);
+            loadedObject.tag = "model3d";
+            error = string.Empty;
+
+            loadedObject = loadedObject.transform.GetChild(0).gameObject;
+
+            //get shader object
+            Shader shader = Shader.Find("Diffuse");
+            //set shader to object
+            loadedObject.GetComponent<Renderer>().material.shader = shader;
+            loadedObject.GetComponent<Renderer>().material.mainTexture = texture;
+            //set color to object
+            loadedObject.GetComponent<Renderer>().material.color = Color.white;
+
+        }
+
+    }
+
+    public void switchView2(){
+        //path = Resources.Load("objSwitch") as string;
+        path[0] = Application.streamingAssetsPath + "/Model3d/" + "wheel_2.obj";
+
+        if (!File.Exists(path[0])){
+            error = "File " + path[0] + " does not exist";
+        }
+        else{
+            if (loadedObject != null){
+                Destroy(loadedObject);
+            }
+            loadedObject = new OBJLoader().Load(path[0]);
+            loadedObject.tag = "model3d";
+            error = string.Empty;
+
+            loadedObject = loadedObject.transform.GetChild(0).gameObject;
+
+            //get shader object
+            Shader shader = Shader.Find("Diffuse");
+            //set shader to object
+            loadedObject.GetComponent<Renderer>().material.shader = shader;
+            loadedObject.GetComponent<Renderer>().material.mainTexture = texture;
+            //set color to object
+            loadedObject.GetComponent<Renderer>().material.color = Color.white;
+
+        }
+
     }
 
 

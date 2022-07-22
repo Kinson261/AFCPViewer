cc10df0a680496d1c9dd768ae97a80c036ad2c38
Kinson261 <80491985+Kinson261@users.noreply.github.com>
2022-07-22 23:05:31 +0300

    Define start time from opening project
    changed time from  void update() to void start()

--- Assets/Scripts/UI/getDateAndTime.cs
+++ Assets/Scripts/UI/getDateAndTime.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class getDateAndTime : MonoBehaviour
 {
 
     public string date;
     // Start is called before the first frame update
-    public void Update() {
+    public void Start() {
         date = System.DateTime.Now.ToString("dd-MM-yyyy_HH-mm");
+        Directory.CreateDirectory(Application.streamingAssetsPath  + "/../../SavedModels/" + date);
     }
 }
